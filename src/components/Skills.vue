5452da0a2f5fdc9af6ec5b62f9fad9a28220746c
Drozerah <drozerah@drozerah.user>
2018-10-15 16:07:30 +0200

    feat: add About.vue component

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -22,13 +22,6 @@
   </div>
 </template>
 
-<template>
-  <div class="about">
-    <h1>This is an about page</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna ali...</p>
-  </div>
-</template>
-
 <script>
 export default {
   name: 'Skills',
